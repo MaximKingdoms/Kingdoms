2eb05627ea82ad9cca57248d755d9cb9dc23f085
MaximKingdoms <MaximJP36@outlook.com>
2026-08-20 03:10:34 -0400

    Integrate MySQL connection in server.js
    Add MySQL connection setup to server.js

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 // Render injectera automatiquement la variable process.env.PORT
 const PORT = process.env.PORT || 3000;
 
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
+});
+
+
 app.get('/', (req, res) => {
     res.send('Mon serveur Node.js est en ligne sur Render !');
 });
