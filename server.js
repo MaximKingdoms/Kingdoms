f5dd557fca736381a9055737d1abb29826467793
Maxim <MaximJP36@outlook.com>
2026-08-20 04:46:23 -0400

    fix passage mysql2 aussi en local

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 // Render injectera automatiquement la variable process.env.PORT
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10, // Nombre maximum de connexions simultanées
+  queueLimit: 0
 });
 
 
@@ -19,6 +24,6 @@ app.get('/', (req, res) => {
     res.send('Mon serveur Node.js est en ligne sur Render !');
 });
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
