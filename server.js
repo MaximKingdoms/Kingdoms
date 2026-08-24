7ff920d96250a0db9e286d7d5bc05915f315f84d
MaximKingdoms <MaximJP36@outlook.com>
2026-08-24 06:06:10 -0400

    Update missile power assignment to use data.hp

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ socket.on('missile', (data) => {
         id: Math.random().toString(36).substring(2, 9), // Identifiant unique utile pour le nettoyage
         x: posX,
         y: posY,
-        power: hp // Optionnel : vitesse de déplacement
+        power: data.hp // Optionnel : vitesse de déplacement
     };
 
     // 4. Ajout du missile dans le tableau
