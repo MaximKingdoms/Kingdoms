a11ddf62cb9203f36dd539547aa7c2005f5f7048
MaximKingdoms <MaximJP36@outlook.com>
2026-08-27 06:05:03 -0400

    Increase monster speed from 30 to 60 pixels per second

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ socket.on('missile', (data) => {
   });
   
 let lastUpdateTime = Date.now();
-const MONSTER_SPEED = 30; // Vitesse en pixels par seconde (ajustez cette valeur)
+const MONSTER_SPEED = 60; // Vitesse en pixels par seconde (ajustez cette valeur)
 
 function moveMonstersServer() {
     const now = Date.now();
