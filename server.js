e97b0eed1c98aee9af9d0805b6c7e85f21ac8c51
MaximKingdoms <MaximJP36@outlook.com>
2026-08-27 04:24:14 -0400

    Increase monster movement frequency to 60ms

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ function moveMonstersServer() {
 }
 
 // 5. Boucle d'exécution du serveur (Ex: 30 fois par seconde ou ~33ms)
-setInterval(moveMonstersServer, 100);
+setInterval(moveMonstersServer, 60);
 
 
   // 5. Gérer la déconnexion d'un joueur
