74fcde202601df8ddbfc95acd6dfcc1dd0ce2627
MaximKingdoms <MaximJP36@outlook.com>
2026-09-01 08:34:01 -0400

    Comment out currentPlayers emission
    Comment out the emission of current players to the requesting socket.

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ io.on('connection', (socket) => {
     console.log("Liste des joueurs envoyée :", listeJoueurs);
     
     // Envoyer la liste uniquement au joueur qui la demande
-socket.emit('currentPlayers', listeJoueurs); 
+//socket.emit('currentPlayers', listeJoueurs); 
   });
 
   // 3. Gestion des dégâts / coups reçus
