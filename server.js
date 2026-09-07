7b7611f0708b9606594a13b3141da1661741495f
MaximKingdoms <MaximJP36@outlook.com>
2026-09-07 13:23:53 -0400

    Fix missile removal condition in server.js

--- server.js
+++ server.js
@@ -314,8 +314,9 @@ socket.on('missile', (data) => {
 
     console.log(`Missile ajouté ! Total en cours : ${listeMissiles.length}`);
         setTimeout(() => {
+                if (nouveaumissile) {
     listeMissiles[nouveaumissile.id].remove();
-    
+                }
     console.log(`Missile détruit ! Total en cours : ${listeMissiles.length}`);
 }, 5000);
 });
