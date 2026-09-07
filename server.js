bf25cd5d828612cebe878aeb618c04bfd45b5ac0
MaximKingdoms <MaximJP36@outlook.com>
2026-09-07 13:31:08 -0400

    Fix missile removal logic in server.js

--- server.js
+++ server.js
@@ -315,7 +315,7 @@ socket.on('missile', (data) => {
     console.log(`Missile ajouté ! Total en cours : ${listeMissiles.length}`);
         setTimeout(() => {
                 if (nouveauMissile) {
-    listeMissiles[nouveauMissile.id].remove();
+    listeMissiles[nouveauMissile].remove();
                 }
     console.log(`Missile détruit ! Total en cours : ${listeMissiles.length}`);
 }, 5000);
