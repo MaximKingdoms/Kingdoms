159e18c7da4304e983d6c0feee6b56d346cecf07
MaximKingdoms <MaximJP36@outlook.com>
2026-09-09 01:15:40 -0400

    Remove monster deletion and client notification logic
    Removed code that deletes monsters when their HP is zero and emits an event to clients.

--- server.js
+++ server.js
@@ -128,13 +128,7 @@ socket.on('hitmonster', (hitdata) => {
       if (monsters[hitdata.monstername]) {
         monsters[hitdata.monstername].power = hitdata.monsterhp;
       }
-          if (hitdata.monsterhp <= 0) {
-            delete monsters[hitdata.monstername];
-            
-            // On informe les clients de le supprimer graphiquement
-            io.emit('monsterRemoved', { id: hitdata.monstername });
         
-      }
 
   });
 
