2a3eb80bdd3cf0904a47e5d496b1095489ceaa51
MaximKingdoms <MaximJP36@outlook.com>
2026-09-09 01:21:08 -0400

    Handle monster removal in hitmonster event
    Emit event to clients when a monster is removed.

--- server.js
+++ server.js
@@ -128,7 +128,13 @@ socket.on('hitmonster', (hitdata) => {
       if (monsters[hitdata.monstername]) {
         monsters[hitdata.monstername].power = hitdata.monsterhp;
       }
+       if (hitdata.isdead == true) {
+            delete monsters[hitdata.monstername];
+            
+            // On informe les clients de le supprimer graphiquement
+            io.emit('monsterRemoved', { id: hitdata.monstername });
         
+      }}
 
   });
 
